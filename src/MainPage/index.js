9cb897902d6525735d785e15457e3c3ffd838f98
ChoiRron <93230997+ChoiRron@users.noreply.github.com>
2022-03-15 08:50:25 +0900

    Add files via upload

---
+++ src/MainPage/index.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Container = styled.div`
+height:100%;
+width:100%;
+display:flex;
+background-color:#28bec6;
+`;
+
+function MainPage() {
+    return (
+        <Container></Container>
+    )
+}
+
+export default MainPage;
