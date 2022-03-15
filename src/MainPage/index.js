741248edf2d1afa45fd196fb9c00aa1b7cc40708
ChoiRron <93230997+ChoiRron@users.noreply.github.com>
2022-03-15 09:36:34 +0900

    Add files via upload

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -3,15 +3,27 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const Container = styled.div`
-height:100%;
-width:100%;
+height:822px;
+width:1440px;
 display:flex;
 background-color:#28bec6;
+color:white;
+font-size:60px;
+font-family:"Times New Roman";
 `;
 
+const Italic = styled.i`
+margin-left:450px;
+padding-top:300px;
+`;
+
+const Hobbies = styled.h2`
+color:white;
+`;
 function MainPage() {
     return (
-        <Container></Container>
+        <Container><Italic>Hello, My Name Is Ron!</Italic></Container>
+        <Hobbies>My Hobbies: Play PUBG  |  Read books  |  Chat with my friends</Hobbies>
     )
 }
 
