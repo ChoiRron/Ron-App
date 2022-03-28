a37aa28b4d0f45475d39e49879c692af8b219d26
ChoiRron <93230997+ChoiRron@users.noreply.github.com>
2022-03-28 09:03:56 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,87 +1,125 @@
 import styled from "styled-components";
-// import React, { useState } from "react";
-// import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>,
-//     document.getElementById('root')
-// );
+// import LoginPage from "./LoginPage";
+// import MainPage from "./MainPage";
+
+const LeftSide = styled.h1`
+ color: blue;
+ text-align: center;
+//  background-color:white;
+ height:100%;
+ width:50%;
+//  line-height:300px;
+`;
+const RightSide = styled.h1`
+background-image: url("https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg");
+background-repeat: no-repeat;
+ font-size: 100px;
+ text-align: center;
+ color:black;
+ height:100%;
+ width:50%;
+ line-height:300px;
+ margin-top:0;
+`;
 
 const Container = styled.div`
-height:822px;
-width:1440px;
-display:flex;
+height:100vh;
 background-color:#28bec6;
+display:flex; 
+`;
+
+const WelcomeTitle = styled.h1`
+color: white;
+font-weight: 800;
+font-size:40px;
+`;
+
+const Label = styled.label`
+font-size:24px;
+color:black;
+`;
+
+const Input = styled.input`
+width:300px;
+height:30px;
+margin-left:30px;
+border:white;
+border-radius:5px;
+`;
+
+const InPut = styled.input`
+width:300px;
+height:30px;
+margin-left:155px;
+margin-top:50px;
+border:white;
+border-radius:5px;
+`;
+
+const LoginButton = styled.button`
+background-color:green;
 color:white;
-font-size:60px;
-font-family:"Times New Roman";
+width:80px;
+height:30px;
+font-size:15px;
+margin-top:50px;
+border-radius:8px;
 `;
+function LoginPage() {
+    const history = useHistory();
 
-const Italic = styled.i`
-margin-left:450px;
-padding-top:300px;
-`;
+    const [user, setUser] = useState({
+        id: "",
+        password: "",
+    });
+    const { id, password } = user;
+    const navigateToMainPage = () => {
+        history.push("/main");
+        if(id==="" || password === "") {
+            alert(`ID or Password is empty.`);
+            return;
+        } else {
+            if(id=="Ron809" && password == "royron310809"){
+                alert(`You may login!`)
+                history.push("/main")
+            }
+        };
+    };// when we first load a page we initialize 
+    //the states to empty string / null / 0 .
+    //states will be filled in when the user does specific action
 
-const MH = styled.h2`
-color:white;
-font-size:30px;
-position:absolute;
-top:450px;
-left:350px;
-`;
+    const putUserInfo = (e) => {//"e" stands for "events"
+        const { name, value } = e.target;
+        console.log(name);
+        console.log(value);
+        setUser({
+            ...user, //combine strings together
+            [name]: value, // to avoid reputatin to using same code
+        });
+        console.log(user);
+    };
 
-const PUBGImg = styled.img`
-background: url("https://img0.baidu.com/it/u=750472804,1731221626&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
-width:200px;
-height:200px;
-background-repeat:no-repeat;
-position:absolute;
-top:550px;
-left:260px;
-line-height:822px;
-`;
+    return (
+        <Container>
+            <LeftSide>
+                <WelcomeTitle>Welcome Back!</WelcomeTitle>
+                <div>
+                    <Label>Enter Your Username</Label>
+                    {/*we are going to replace empty string to the following input by the user*/}
+                    <Input type="text" value={id} name="id" onChange={putUserInfo} placeholder="👨‍🎓 Enter your username"></Input>
+                </div>
+                <div>
+                    <Label>Password</Label>
+                    <InPut type="password" value={password} name="password" onChange={putUserInfo} placeholder="🔑 Enter your password"></InPut>
+                </div>
+                <LoginButton onClick={navigateToMainPage}>Login</LoginButton>
+            </LeftSide>
+            <RightSide></RightSide>
+        </Container>
+    )
+}
 
-const BookImg = styled.img`
-background:url("https://img2.baidu.com/it/u=3639517522,2392425939&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
-width:200px;
-height:200px;
-background-repeat:no-repeat;
-position:absolute;
-top:550px;
-left:620px;
-`;
-
-const ChatingImg = styled.img`
-background:url("https://img2.baidu.com/it/u=3080209464,2374011713&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
-width:200px;
-height:200px;
-background-repeat:no-repeat;
-position:absolute;
-top:550px;
-left:980px;
-`;
-
-const RonImg = styled.img`
-background:url("https://i.insider.com/568d4264dd0895a83d8b45eb?width=1048&format=jpeg");
-width:400px;
-height:200px;
-background-repeat:no-repeat;
-position:absolute;
-top:30px;
-left:580px;
-`;
-function MainPage() {
-return (
-    <Container><Italic>Hello, My Name Is Ron!</Italic>
-        <RonImg></RonImg>
-        <MH>My Hobbies:Play PUBG | Read books | Chat with my friends</MH>
-        <a href="https://www.google.com/search?q=pubg&source=lmns&bih=746&biw=1309&hl=zh-CN&sa=X&ved=2ahUKEwir66Gfvef2AhXWRvUHHVSwAXwQ_AUoAHoECAEQAA"><PUBGImg></PUBGImg></a>
-        <a href="https://www.amazon.com/books-used-books-textbooks/b?ie=UTF8&node=283155"><BookImg></BookImg></a>
-        <a href="https://www.google.com/search?q=kakaotalk&oq=kakaotalk&aqs=chrome..69i57j0i512l9.2296j1j7&sourceid=chrome&ie=UTF-8"><ChatingImg></ChatingImg></a>
-    </Container>
-)
-};
-
-export default MainPage;
+export default LoginPage;
