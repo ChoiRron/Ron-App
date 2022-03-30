960aca11e507760ef3d47129ac0da28eed134c13
ChoiRron <2027ronchoi@usbccollegiate.org>
2022-03-30 10:06:01 +0900

    quize

--- src/LearnPage/index.js
+++ src/LearnPage/index.js
@@ -24,28 +24,46 @@ margin-left:610px;
 const Detail = styled.p`
 position:absolute;
 margin-top:280px;
-margin-left:
+margin-left:610px;
 `;
 
 const Info = styled.p`
-
+position:absolute;
+margin-top:450px;
+margin-left:610px;
+color:#8338ec;
 `;
 
+const InfoC = styled.p`
+position:absolute;
+margin-top:450px;
+margin-left:900px;
+color:#8338ec;
+`;
 const ImgAboutMe = styled.img`
-
+width:350px;
+height:250px;
+position:absolute;
+margin-top:200px;
+margin-left:130px;
 `;
 function LearnPage(){
     return(
         <Container>
             <Topic>About me</Topic>
             <SubTopic>Hello, I am Ron Choi. I am learning <br></br>Web Development, and I am trying <br></br>to make my own web site now.</SubTopic>
-            <Detail>I have been learning for Web Development for a while, and I have made many web pages before, and now, I am trying to make a new react page! React page is more complicated than a web page.</Detail>
-            {/* <Info>Name:                                             Ron Choi
-                <br></br>Email:                                             2027ronchoi@usbccollegiate.org
-                <br></br>Phone:                                             010 0000 0000
-                <br></br>Birth:                                             August 9th 2009
+            <Detail>I have been learning for Web Development for a while,<br></br> and I have made many web pages before, and now, I am <br></br>trying to make a new react page! React page is more<br></br> complicated than a web page.</Detail>
+            <Info>Name:
+                <br></br>Email:
+                <br></br>Phone:
+                <br></br>Birth:
                 </Info>
-            <ImgAboutMe src="https://i.pinimg.com/originals/fc/ae/d8/fcaed8e309e8cd7ebd3777a13df667f8.jpg"></ImgAboutMe> */}
+            <InfoC>Ron Choi
+                <br></br>2027ronchoi@usbccollegiate.org
+                <br></br>010 0000 0000
+                <br></br>August 9th 2009
+            </InfoC>
+            <ImgAboutMe src="https://cdn.w600.comps.canstockphoto.com/it-computer-with-book-drawing_csp1661654.jpg"></ImgAboutMe>
         </Container>
     )
 };
